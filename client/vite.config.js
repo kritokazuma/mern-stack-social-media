fcb4dcac3e50f00c1029c1de26d56afc0b0dadb7
kritokazuma <demoinerl2h@gmail.com>
2021-12-14 00:04:19 +0630

    server changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://shrouded-atoll-71846.herokuapp.com/",
         rewrite: (path) => path.replace("/^/api/", ""),
       },
       "/ws": {
