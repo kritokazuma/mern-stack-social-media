801b28513a394a1ad4405b6d5dbe8c4c9768bc6c
kritokazuma <demoinerl2h@gmail.com>
2021-12-13 23:39:56 +0630

    make production test

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://mern-stack-social-media.vercel.app",
+    ],
   },
 });
 
