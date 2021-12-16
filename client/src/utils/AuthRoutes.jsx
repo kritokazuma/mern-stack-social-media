4325408814887472b0ee8f3229f736ca00b769bd
kritokazuma <demoinerl2h@gmail.com>
2021-12-16 18:29:44 +0630

    release

--- client/src/utils/AuthRoutes.jsx
+++ client/src/utils/AuthRoutes.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Route, Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 
 export function AuthRoutes({ children }) {
